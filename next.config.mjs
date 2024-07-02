1009c4e06401e5c6f272f6bb0e5eb9d6a03534c9
wlsdud0448 <wlsdud0448@naver.com>
2024-07-02 13:24:26 +0900

    0702_first_init_update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     env: {
         NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
         NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
